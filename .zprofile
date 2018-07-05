89c93c19b8cacd9598825b2cc6adb83fc7fa2060
ghetto <ghetto.ch@gmail.com>
2018-07-05 18:00:43 +0200

    small fixes

--- .zprofile
+++ .zprofile
@@ -1,8 +1,19 @@
+if ! mount|grep ownCloud > /dev/null; then
+    mount /mnt/ownCloud
+fi
 
-mount /mnt/ownCloud
-mount /mnt/Data
-mount /mnt/Movies
-mount /mnt/Series
+if ! mount|grep Data > /dev/null; then
+    mount /mnt/Data
+fi
+
+if ! mount|grep Movies > /dev/null; then
+    mount /mnt/Movies
+fi
+
+if ! mount|grep Series > /dev/null; then
+    mount /mnt/Series
+fi
+
 
 emulate sh -c 'source /etc/profile'
 [[ ! $DISPLAY && $XDG_VTNR -eq 1 && $(id --group) -ne 0 ]] && exec startx
--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -147,14 +147,14 @@ function open_command() {
 }
 
 # Show dots while waiting for tab-completion
-expand-or-complete-with-dots() {
-	# toggle line-wrapping off and back on again
-	[[ -n "$terminfo[rmam]" && -n "$terminfo[smam]" ]] && echoti rmam
-	print -Pn "%{%F{red}......%f%}"
-	[[ -n "$terminfo[rmam]" && -n "$terminfo[smam]" ]] && echoti smam
-
-	zle expand-or-complete
-	zle redisplay
-}
-zle -N expand-or-complete-with-dots
-bindkey "^I" expand-or-complete-with-dots
+#expand-or-complete-with-dots() {
+#	# toggle line-wrapping off and back on again
+#	[[ -n "$terminfo[rmam]" && -n "$terminfo[smam]" ]] && echoti rmam
+#	print -Pn "%{%F{red}......%f%}"
+#	[[ -n "$terminfo[rmam]" && -n "$terminfo[smam]" ]] && echoti smam
+#
+#	zle expand-or-complete
+#	zle redisplay
+#}
+#zle -N expand-or-complete-with-dots
+#bindkey "^I" expand-or-complete-with-dots
