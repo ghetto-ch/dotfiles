8ad17e1df9a17a0b23f6407f13e6013530ea6485
ghetto <ghetto.ch@gmail.com>
2018-11-11 09:08:03 +0100

    update

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+i3-msg "workspace 9; append_layout /home/ghetto/.config/i3/ws-term.json" &
+st -t htop htop &
+st -t sensors watch -n 5 sensors &
+st -t iostat watch -n 5 iostat &
+st -t gpu sudo radeontop &
+sleep 1
 i3-msg "workspace 1; append_layout /home/ghetto/.config/i3/ws-main.json" &
 firefox &
 emacs &
