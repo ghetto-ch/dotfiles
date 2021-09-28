1097c43dbdc9e5ee9fced6d1c1f7757d3e54563f
ghetto <ghetto.ch@gmail.com>
2021-09-28 13:07:29 +0200

    wsl clipboard

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -3,6 +3,26 @@ local o = vim.opt
 local c = vim.cmd
 local map = vim.api.nvim_set_keymap
 
+local is_wsl = (function ()
+	return string.find((vim.fn.systemlist 'uname -r')[1], 'WSL')
+end)
+
+if is_wsl then
+	g.clipboard = [[
+	'name': 'wsl-clip',
+	'copy': {
+		'+': 'clip.exe',
+		'*': 'clip.exe',
+	},
+	'paste': {
+		'+': "powershell.exe Get-Clipboard",
+		'*': "powershell.exe Get-Clipboard",
+	},
+	'cache_enabled': 0,
+}
+	]]
+end
+
 g.mapleader = ' '
 o.termguicolors = true
 o.background = 'dark'
