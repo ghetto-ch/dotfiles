16add3916180512a7f2f76df0efc86f77e94b69e
ghetto <ghetto.ch@gmail.com>
2021-10-16 09:50:37 +0200

    stylua

--- .config/nvim/plugin/plugins.lua
+++ .config/nvim/plugin/plugins.lua
@@ -17,190 +17,201 @@ vim.cmd([[
 	augroup end
 ]])
 
-require('packer').startup({function(use)
-	use({ 'wbthomason/packer.nvim' })
+require('packer').startup({
+	function(use)
+		use({ 'wbthomason/packer.nvim' })
 
-	-- Common use
-	use({ 'nvim-lua/plenary.nvim' })
+		-- Common use
+		use({ 'nvim-lua/plenary.nvim' })
 
-	use({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
-	use({
-		'nvim-telescope/telescope.nvim',
-		opt = true,
-		cmd = { 'Telescope', 'Teledot', 'Televim' },
-		requires = {
-			{ 'nvim-lua/plenary.nvim' },
-		},
-		config = function()
-			require('tele')
-		end,
-	})
+		use({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
+		use({
+			'nvim-telescope/telescope.nvim',
+			opt = true,
+			cmd = { 'Telescope', 'Teledot', 'Televim' },
+			requires = {
+				{ 'nvim-lua/plenary.nvim' },
+			},
+			config = function()
+				require('tele')
+			end,
+		})
 
-	use({ 'kyazdani42/nvim-web-devicons' })
+		use({ 'kyazdani42/nvim-web-devicons' })
 
-	use({ 'christoomey/vim-tmux-navigator' })
+		use({ 'christoomey/vim-tmux-navigator' })
 
-	use({ 'famiu/bufdelete.nvim', opt = true, event = { 'BufReadPost' } })
+		use({ 'famiu/bufdelete.nvim', opt = true, event = { 'BufReadPost' } })
 
-	use({
-		'norcalli/nvim-colorizer.lua',
-		opt = true,
-		event = { 'BufReadPre', 'BufRead', 'BufNew' },
-		config = function()
-			require('colorizer').setup()
-		end,
-	})
+		use({
+			'nacro90/numb.nvim',
+			opt = true,
+			event = { 'BufReadPost' },
+			config = function()
+				require('numb').setup()
+			end,
+		})
 
-	use({
-		'unblevable/quick-scope',
-		config = function()
-			vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
-		end,
-	})
+		use({
+			'norcalli/nvim-colorizer.lua',
+			opt = true,
+			event = { 'BufReadPre', 'BufRead', 'BufNew' },
+			config = function()
+				require('colorizer').setup()
+			end,
+		})
 
-	use({ 'ghetto-ch/vim-noh', opt = true, event = { 'BufReadPost' } })
+		use({
+			'unblevable/quick-scope',
+			config = function()
+				vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
+			end,
+		})
 
-	use({
-		'RRethy/nvim-align',
-		opt = true,
-		event = { 'BufReadPost' },
-		config = function()
-			vim.cmd(
-				'command! -range=% -nargs=1 Align lua require("align").align(<f-args>)'
-			)
-		end,
-	})
+		use({ 'ghetto-ch/vim-noh', opt = true, event = { 'BufReadPost' } })
 
-	-- General for programming
-	use({
-		'tpope/vim-surround',
-		opt = true,
-		event = { 'BufReadPost' },
-		requires = { 'tpope/vim-repeat' },
-	})
+		use({
+			'RRethy/nvim-align',
+			opt = true,
+			event = { 'BufReadPost' },
+			config = function()
+				vim.cmd(
+					'command! -range=% -nargs=1 Align lua require("align").align(<f-args>)'
+				)
+			end,
+		})
 
-	use({
-		'tpope/vim-commentary',
-		opt = true,
-		event = { 'BufReadPost' },
-		requires = { 'tpope/vim-repeat' },
-	})
+		-- General for programming
+		use({
+			'tpope/vim-surround',
+			opt = true,
+			event = { 'BufReadPost' },
+			requires = { 'tpope/vim-repeat' },
+		})
 
-	use({
-		'windwp/nvim-autopairs',
-		opt = true,
-		after = 'nvim-cmp',
-		config = function()
-			require('autopairs')
-		end,
-	})
+		use({
+			'tpope/vim-commentary',
+			opt = true,
+			event = { 'BufReadPost' },
+			requires = { 'tpope/vim-repeat' },
+		})
 
-	use({
-		'jpalardy/vim-slime',
-		opt = true,
-		event = { 'BufReadPost' },
-		config = function()
-			require('slime')
-		end,
-	})
+		use({
+			'windwp/nvim-autopairs',
+			opt = true,
+			after = 'nvim-cmp',
+			config = function()
+				require('autopairs')
+			end,
+		})
 
-	use({
-		'lewis6991/gitsigns.nvim',
-		opt = true,
-		event = { 'BufReadPost' },
-		requires = { 'nvim-lua/plenary.nvim' },
-		config = function()
-			require('signs')
-		end,
-	})
+		use({
+			'jpalardy/vim-slime',
+			opt = true,
+			event = { 'BufReadPost' },
+			config = function()
+				require('slime')
+			end,
+		})
 
-	use({
-		'sindrets/diffview.nvim',
-		opt = true,
-		event = { 'BufReadPost' },
-		config = function()
-			require('diff')
-		end,
-	})
+		use({
+			'lewis6991/gitsigns.nvim',
+			opt = true,
+			event = { 'BufReadPost' },
+			requires = { 'nvim-lua/plenary.nvim' },
+			config = function()
+				require('signs')
+			end,
+		})
 
-	-- Debug with gdb etc...
-	use({
-		'sakhnik/nvim-gdb',
-		opt = true,
-		event = { 'BufReadPost' },
-		run = './install.sh',
-	})
+		use({
+			'sindrets/diffview.nvim',
+			opt = true,
+			event = { 'BufReadPost' },
+			config = function()
+				require('diff')
+			end,
+		})
 
-	-- Text objects
-	use({ 'wellle/targets.vim', opt = true, event = { 'BufReadPost' } })
+		-- Debug with gdb etc...
+		use({
+			'sakhnik/nvim-gdb',
+			opt = true,
+			event = { 'BufReadPost' },
+			run = './install.sh',
+		})
 
-	use({
-		'nvim-treesitter/nvim-treesitter-textobjects',
-		opt = true,
-		after = 'nvim-treesitter',
-		branch = '0.5-compat',
-	})
+		-- Text objects
+		use({ 'wellle/targets.vim', opt = true, event = { 'BufReadPost' } })
 
-	use({
-		'RRethy/nvim-treesitter-textsubjects',
-		opt = true,
-		after = 'nvim-treesitter',
-	})
+		use({
+			'nvim-treesitter/nvim-treesitter-textobjects',
+			opt = true,
+			after = 'nvim-treesitter',
+			branch = '0.5-compat',
+		})
 
-	-- Completion and snippets
-	use({
-		'neovim/nvim-lspconfig',
-		opt = true,
-		after = 'nvim-cmp',
-		config = function()
-			require('lsp')
-		end,
-	})
+		use({
+			'RRethy/nvim-treesitter-textsubjects',
+			opt = true,
+			after = 'nvim-treesitter',
+		})
 
-	use({ 'onsails/lspkind-nvim' })
+		-- Completion and snippets
+		use({
+			'neovim/nvim-lspconfig',
+			opt = true,
+			after = 'nvim-cmp',
+			config = function()
+				require('lsp')
+			end,
+		})
 
-	use({
-		'RRethy/vim-illuminate',
-		opt = true,
-		event = { 'BufReadPost' },
-		config = function()
-			vim.g.Illuminate_delay = 500
-		end,
-	})
+		use({ 'onsails/lspkind-nvim' })
 
-	use({
-		'hrsh7th/nvim-cmp',
-		opt = true,
-		event = { 'BufReadPre', 'BufRead', 'BufNew' },
-		requires = {
-			'saadparwaiz1/cmp_luasnip',
-			'hrsh7th/cmp-path',
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-nvim-lua',
-		},
-		config = function()
-			require('nvim-cmp')
-		end,
-	})
+		use({
+			'RRethy/vim-illuminate',
+			opt = true,
+			event = { 'BufReadPost' },
+			config = function()
+				vim.g.Illuminate_delay = 500
+			end,
+		})
 
-	use({
-		'L3MON4D3/LuaSnip',
-		opt = true,
-		after = 'nvim-cmp',
-		config = function()
-			require('snip')
-		end,
-	})
+		use({
+			'hrsh7th/nvim-cmp',
+			opt = true,
+			event = { 'BufReadPre', 'BufRead', 'BufNew' },
+			requires = {
+				'saadparwaiz1/cmp_luasnip',
+				'hrsh7th/cmp-path',
+				'hrsh7th/cmp-nvim-lsp',
+				'hrsh7th/cmp-nvim-lua',
+			},
+			config = function()
+				require('nvim-cmp')
+			end,
+		})
 
-	use({
-		'nvim-treesitter/nvim-treesitter',
-		opt = true,
-		event = { 'BufRead', 'BufNew' },
-		branch = '0.5-compat',
-		run = ':TSUpdate',
-		requires = { 'nvim-treesitter/playground', opt = true },
-		config = function()
-			require('treesitter')
-		end,
-	})
-end,})
+		use({
+			'L3MON4D3/LuaSnip',
+			opt = true,
+			after = 'nvim-cmp',
+			config = function()
+				require('snip')
+			end,
+		})
+
+		use({
+			'nvim-treesitter/nvim-treesitter',
+			opt = true,
+			event = { 'BufRead', 'BufNew' },
+			branch = '0.5-compat',
+			run = ':TSUpdate',
+			requires = { 'nvim-treesitter/playground', opt = true },
+			config = function()
+				require('treesitter')
+			end,
+		})
+	end,
+})
