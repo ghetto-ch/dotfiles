8ad5eb9771e6ba62d95d626886861d1e0dca151f
ghetto <ghetto.ch@gmail.com>
2024-05-30 21:37:15 +0200

    update trouble.nvim keybinds

--- .config/nvim/lua/user/plugins/trouble.lua
+++ .config/nvim/lua/user/plugins/trouble.lua
@@ -4,14 +4,25 @@ return {
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	keys = {
 		{
-			'<leader>xx',
+			'<leader>td',
 			'<cmd>Trouble diagnostics toggle<cr>',
 			desc = 'Diagnostics (Trouble)',
 		},
+		{
+			'<leader>ts',
+			'<cmd>Trouble symbols toggle focus=false<cr>',
+			desc = 'Symbols (Trouble)',
+		},
+		{
+			'<leader>tl',
+			'<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+			desc = 'LSP Definitions / references / ... (Trouble)',
+		},
+		{
+			'<leader>tq',
+			'<cmd>Trouble qflist toggle<cr>',
+			desc = 'Quickfix List (Trouble)',
+		},
 	},
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
+	opts = {},
 }
