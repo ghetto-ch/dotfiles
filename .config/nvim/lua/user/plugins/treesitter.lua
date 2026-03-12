21631733612da7d87683d29ce8fe1b1f16f6ba44
ghetto <ghetto.ch@gmail.com>
2026-03-12 14:33:36 +0100

    update treesitter textobjects

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -1,51 +1,13 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	-- event = { 'BufReadPre', 'BufNewFile' },
 	build = ":TSUpdate",
 	branch = "main",
 	dependencies = {
 		{ "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
-		-- 'RRethy/nvim-treesitter-textsubjects',
 	},
 
 	config = function()
-		-- local treesitter = require('nvim-treesitter.configs')
-
 		require("nvim-treesitter").setup({
-
-			textobjects = {
-				select = {
-					enable = true,
-					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-					},
-				},
-			},
-
-			move = {
-				enable = true,
-				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next_start = {
-					["]f"] = "@function.outer",
-					["]c"] = "@class.outer",
-				},
-				goto_next_end = {
-					["]F"] = "@function.outer",
-					["]C"] = "@class.outer",
-				},
-				goto_previous_start = {
-					["[f"] = "@function.outer",
-					["[c"] = "@class.outer",
-				},
-				goto_previous_end = {
-					["[F"] = "@function.outer",
-					["[C"] = "@class.outer",
-				},
-			},
 
 			require("nvim-treesitter").install({
 				"lua",
