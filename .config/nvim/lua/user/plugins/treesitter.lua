4e6f993466570567de0dfbec4a7ee40a495e714d
ghetto <ghetto.ch@gmail.com>
2026-03-13 13:08:45 +0100

    simplify treesitter.lua

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -1,19 +1,16 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	build = ':TSUpdate',
-	branch = 'main',
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	branch = "main",
 
 	config = function()
-		require('nvim-treesitter').setup({
-
-			require('nvim-treesitter').install({
-				'lua',
-				'vimdoc',
-				'c',
-				'bash',
-				'python',
-				'fish',
-			}),
+		require("nvim-treesitter").install({
+			"lua",
+			"vimdoc",
+			"c",
+			"bash",
+			"python",
+			"fish",
 		})
 	end,
 }
