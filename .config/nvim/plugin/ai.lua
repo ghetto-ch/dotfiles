33468ef8f41125bad8d2109ebb29dfacb1fdf0fc
ghetto <ghetto.ch@gmail.com>
2026-04-28 20:15:27 +0200

    remove unused stuff

--- .config/nvim/plugin/ai.lua
+++ .config/nvim/plugin/ai.lua
@@ -2,38 +2,29 @@
 -- Opencode
 --#############################################################################
 
-vim.g.opeocode_opts = {
-	server = {
-		url = "http://localhost", -- o 'http://127.0.0.1'
-		port = 4096, -- deve corrispondere alla porta scelta sopra
-		timeout = 10,
-	},
-	opencode_executable = "opencode", -- assicurati che sia nel tuo PATH
-}
+vim.keymap.set({ 'n', 'x' }, '<leader>oa', function()
+	require('opencode').ask('@this: ', { submit = true })
+end, { desc = 'Ask opencode…' })
+vim.keymap.set({ 'n', 'x' }, '<leader>ox', function()
+	require('opencode').select()
+end, { desc = 'Execute opencode action…' })
+vim.keymap.set({ 'n', 't' }, '<leader>ot', function()
+	require('opencode').toggle()
+end, { desc = 'Toggle opencode' })
 
-vim.keymap.set({ "n", "x" }, "<leader>oa", function()
-	require("opencode").ask("@this: ", { submit = true })
-end, { desc = "Ask opencode…" })
-vim.keymap.set({ "n", "x" }, "<leader>ox", function()
-	require("opencode").select()
-end, { desc = "Execute opencode action…" })
-vim.keymap.set({ "n", "t" }, "<leader>ot", function()
-	require("opencode").toggle()
-end, { desc = "Toggle opencode" })
+vim.keymap.set({ 'n', 'x' }, 'go', function()
+	return require('opencode').operator('@this ')
+end, { desc = 'Add range to opencode', expr = true })
+vim.keymap.set('n', 'goo', function()
+	return require('opencode').operator('@this ') .. '_'
+end, { desc = 'Add line to opencode', expr = true })
 
-vim.keymap.set({ "n", "x" }, "go", function()
-	return require("opencode").operator("@this ")
-end, { desc = "Add range to opencode", expr = true })
-vim.keymap.set("n", "goo", function()
-	return require("opencode").operator("@this ") .. "_"
-end, { desc = "Add line to opencode", expr = true })
-
-vim.keymap.set("n", "<S-C-u>", function()
-	require("opencode").command("session.half.page.up")
-end, { desc = "Scroll opencode up" })
-vim.keymap.set("n", "<S-C-d>", function()
-	require("opencode").command("session.half.page.down")
-end, { desc = "Scroll opencode down" })
+vim.keymap.set('n', '<S-C-u>', function()
+	require('opencode').command('session.half.page.up')
+end, { desc = 'Scroll opencode up' })
+vim.keymap.set('n', '<S-C-d>', function()
+	require('opencode').command('session.half.page.down')
+end, { desc = 'Scroll opencode down' })
 
 --#############################################################################
 -- Copilot
@@ -41,23 +32,23 @@ end, { desc = "Scroll opencode down" })
 
 -- Wrap setup in an autocmd because the copilot server takes long to startup.
 -- Not ideal because :Copilot will not be available before.
-vim.api.nvim_create_autocmd("InsertEnter", {
+vim.api.nvim_create_autocmd('InsertEnter', {
 	once = true,
 	callback = function()
-		require("copilot").setup({
+		require('copilot').setup({
 			panel = {
 				enabled = true,
 				auto_refresh = false,
 				-- TODO find good keymaps
 				keymap = {
-					jump_prev = "[[",
-					jump_next = "]]",
-					accept = "<CR>",
-					refresh = "gr",
-					open = "<M-CR>",
+					jump_prev = '[[',
+					jump_next = ']]',
+					accept = '<CR>',
+					refresh = 'gr',
+					open = '<M-CR>',
 				},
 				layout = {
-					position = "right", -- | top | left | right | bottom |
+					position = 'right', -- | top | left | right | bottom |
 					ratio = 0.4,
 				},
 			},
@@ -69,25 +60,25 @@ vim.api.nvim_create_autocmd("InsertEnter", {
 				trigger_on_accept = true,
 				-- TODO find good keymaps
 				keymap = {
-					accept = "<M-y>",
+					accept = '<M-y>',
 					accept_word = false,
 					accept_line = false,
-					next = "<M-n>",
-					prev = "<M-p>",
-					dismiss = "",
+					next = '<M-n>',
+					prev = '<M-p>',
+					dismiss = '',
 					toggle_auto_trigger = false,
 				},
 			},
 		})
 
 		-- Map a key combination to toggle Copilot auto_trigger
-		vim.keymap.set("n", "<leader>ct", function()
-			require("copilot.suggestion").toggle_auto_trigger()
-		end, { desc = "Toggle Copilot" })
+		vim.keymap.set('n', '<leader>ct', function()
+			require('copilot.suggestion').toggle_auto_trigger()
+		end, { desc = 'Toggle Copilot' })
 
 		-- Map a key combination to toggle Copilot panel
-		vim.keymap.set("n", "<leader>cp", function()
-			require("copilot.panel").toggle()
-		end, { desc = "Toggle Copilot" })
+		vim.keymap.set('n', '<leader>cp', function()
+			require('copilot.panel').toggle()
+		end, { desc = 'Toggle Copilot' })
 	end,
 })
