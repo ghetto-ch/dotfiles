2b4a3ae039a2c7045e150a05bf45a9ad23126989
ghetto <ghetto.ch@gmail.com>
2026-04-30 13:02:20 +0200

    vim.hl

--- .config/nvim/plugin/autocmd.lua
+++ .config/nvim/plugin/autocmd.lua
@@ -1,15 +1,15 @@
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+autocmd('TextYankPost', {
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
 })
 
 -- open help in vertical split
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "help",
-	command = "wincmd L",
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = 'help',
+	command = 'wincmd L',
 })
