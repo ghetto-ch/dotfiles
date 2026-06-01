869376c3d9e0a9b45db1e8ed5ba2525845653997
ghetto <ghetto.ch@gmail.com>
2026-06-01 20:56:05 +0200

    disable treesitter indentation

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -7,4 +7,4 @@ vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.wo[0][0].foldmethod = 'expr'
 vim.wo[0][0].foldlevel = 99
 
-vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+-- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
