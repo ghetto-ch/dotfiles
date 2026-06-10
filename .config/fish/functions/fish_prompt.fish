c33a19ee8830a65a62cc98fe7559a3242c15df40
ghetto <ghetto.ch@gmail.com>
2026-06-10 20:24:21 +0200

    better git status in fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -7,35 +7,37 @@ function fish_prompt
     printf '%s ' (prompt_pwd)
     set_color normal
 
-    # Fish git prompt
-    set __fish_git_prompt_showdirtystate yes
-    set __fish_git_prompt_showstashstate yes
-    set __fish_git_prompt_showuntrackedfiles yes
-    set __fish_git_prompt_showupstream yes
-    set __fish_git_prompt_color_branch yellow
-    set __fish_git_prompt_color_upstream_ahead green
-    set __fish_git_prompt_color_upstream_behind red
-
-    # Status Chars
-    set __fish_git_prompt_char_dirtystate '⚡'
-    set __fish_git_prompt_char_stagedstate '→'
-    set __fish_git_prompt_char_untrackedfiles '☡'
-    set __fish_git_prompt_char_stashstate '↩'
-    set __fish_git_prompt_char_upstream_ahead '+'
-    set __fish_git_prompt_char_upstream_behind -
-
     function git_status
-        set last_status $status
-        printf '%s ' (__fish_git_prompt)
+        set -g __fish_git_prompt_show_informative_status 1
+        set -g __fish_git_prompt_color_branch brmagenta
+        set -g __fish_git_prompt_showupstream informative
+        set -g __fish_git_prompt_showdirtystate yes
+        set -g __fish_git_prompt_color_stagedstate yellow
+        set -g __fish_git_prompt_color_invalidstate red
+        set -g __fish_git_prompt_color_cleanstate brgreen
+
+        # Status Chars
+        set -g __fish_git_prompt_char_dirtystate '⚡'
+        set -g __fish_git_prompt_char_stagedstate '→'
+        set -g __fish_git_prompt_char_untrackedfiles '☡'
+        set -g __fish_git_prompt_char_stashstate '↩'
+        set -g __fish_git_prompt_char_upstream_ahead '+'
+        set -g __fish_git_prompt_char_upstream_behind -
+
+        printf '%s%s%s%s%s%s ' (fish_git_prompt)
         set_color normal
     end
 
-    set_color green
+    # set_color green
     git_status
-    set_color normal
+    # set_color normal
 
     # Line 2
     echo
+    if set -q SSH_TTY
+        # set -g fish_color_host brred
+        printf '(ssh) '
+    end
     printf 'λ ' # ↪
     set_color normal
 
