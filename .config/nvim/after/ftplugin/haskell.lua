8488ed1d967183bb8ae72f28cbc7fdfa318f0a8d
ghetto <ghetto.ch@gmail.com>
2026-06-17 05:09:05 +0200

    haskell treesitter now fixed

--- .config/nvim/after/ftplugin/haskell.lua
+++ .config/nvim/after/ftplugin/haskell.lua
@@ -4,7 +4,7 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
 
 vim.lsp.enable('hls')
--- vim.treesitter.start()
+vim.treesitter.start()
 
 -- vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 -- vim.wo[0][0].foldmethod = 'expr'
