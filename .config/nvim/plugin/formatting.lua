1d8fea24862bd176646307e0e1293331149ccb03
ghetto <ghetto.ch@gmail.com>
2026-07-09 10:32:57 +0200

    fix formatters for kdl and toml

--- .config/nvim/plugin/formatting.lua
+++ .config/nvim/plugin/formatting.lua
@@ -11,11 +11,18 @@ vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
 				fish = { 'fish_indent' },
 				nix = { 'nixpkgs_fmt', lsp_format = 'never' },
 				yaml = { 'yamlfmt', 'prettier' },
-				toml = { 'taplo fmt' },
-				-- kdl = { 'kdlfmt' },
+				toml = { 'taplo' },
+				kdl = { 'kdlfmt' },
 				json = { 'prettier' },
 				rust = { 'rustfmt' },
 				haskell = { 'fourmolu' },
+			},
+			formatters = {
+				-- v1 used by niri.
+				-- Not ideal to specify this here but is my only use case currently
+				kdlfmt = {
+					args = { 'format', '--kdl-version', 'v1', '-' },
+				},
 			},
 			format_on_save = {
 				lsp_fallback = true,
