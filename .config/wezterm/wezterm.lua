4fa87303f9a6d2882cb51175fafb6483289cd72f
ghetto <ghetto.ch@gmail.com>
2026-07-12 16:45:33 +0200

    use ligatures in wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 local act = wezterm.action
 
-config.font = wezterm.font('Hack Nerd Font Mono')
+config.font = wezterm.font('Hack FC Ligatured')
 config.font_size = 13.0
 
 require('colors.kanagawa-gh').apply(config)
