a2ab48b32468833935d1994e920df80f19707091
ghetto <ghetto.ch@gmail.com>
2026-07-14 10:00:29 +0200

    remove comment

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,7 +17,6 @@ vim.g.loaded_tarPlugin = 1
 vim.g.loaded_zipPlugin = 1
 vim.g.loaded_tutor_mode_plugin = 1
 vim.g.loaded_remote_plugins = 1
--- oil replaces netrw (see plugin/filemanager.lua)
 vim.g.loaded_netrwPlugin = 1
 
 -- Hooks for plugins
