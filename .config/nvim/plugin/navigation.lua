3dbebfcbb299eacb2e9db0def8ed98ddce9b8135
ghetto <ghetto.ch@gmail.com>
2026-07-15 15:41:58 +0200

    lazy flash and eyeliner

--- .config/nvim/plugin/navigation.lua
+++ .config/nvim/plugin/navigation.lua
@@ -1,6 +1,7 @@
 vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
 	once = true,
 	callback = function()
+		vim.cmd.packadd('flash.nvim')
 		require('flash').setup({
 			modes = {
 				search = {
@@ -12,6 +13,7 @@ vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
 			},
 		})
 
+		vim.cmd.packadd('eyeliner.nvim')
 		require('eyeliner').setup({})
 		vim.api.nvim_set_hl(0, 'EyelinerPrimary', { fg = '#BF0000', bold = true })
 		vim.api.nvim_set_hl(0, 'EyelinerSecondary', { fg = '#BF0000' })
