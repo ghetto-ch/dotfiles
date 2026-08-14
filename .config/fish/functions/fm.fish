5b32116e3bf14707f17a4e9c2d2bff8b6a7b5332
ghetto <ghetto.ch@gmail.com>
2026-08-14 11:29:14 +0200

    fm function with change of cwd. Yazi specific.

--- .config/fish/functions/fm.fish
+++ .config/fish/functions/fm.fish
@@ -1,3 +1,13 @@
-function fm --description 'Launch file manager'
-    $CLI_FILEMANAGER $argv
+# function fm --description 'Launch file manager'
+#     $CLI_FILEMANAGER $argv
+# end
+
+function fm
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+        builtin cd -- "$cwd"
+    end
+    command rm -f -- "$tmp"
+    commandline -f repaint
 end
